90cdee21162bd4dee005abd88a38af1cef785625
Mouhcine Daali <daalim277@gmail.com>
2022-03-24 23:05:36 +0100

    delete the code to start again

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 
 function App() {
-    const title = 'Post'
-    const paragraph = 'Welcome to the post page.'
-    const comments = [
-        {id: 1, text: 'this service is awesome.'},
-        {id: 2, text: 'i didn\'t like this service.'},
-        {id: 3, text: 'it was good.'},
-        {id: 4, text: 'not something special.'}
-    ]
     return (
-        <div>
-            <h1 className="blue">{title}</h1>
-            <p className="red">{paragraph}</p>
-            <h3>Comments ({comments.lenght}):</h3>
-            
-            <div>
-                <ul>
-                    {comments.map( comment => {
-                        <li>{comment.text}</li>
-                    })}   
-                </ul>
-
-            </div>
-            
-        </div> 
+        <>
+            <h1>My App</h1>
+        </>
     )
 }
 
