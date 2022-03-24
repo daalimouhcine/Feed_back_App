28df3fc896dd03f8ea219f54461ccc6bbb30345d
Mouhcine Daali <daalim277@gmail.com>
2022-03-24 22:35:26 +0100

    trying to practis

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 
 function App() {
+    const title = 'Post'
+    const paragraph = 'Welcome to the post page.'
+    const comments = [
+        {id: 1, text: 'this service is awesome.'},
+        {id: 2, text: 'i didn\'t like this service.'},
+        {id: 3, text: 'it was good.'},
+        {id: 4, text: 'not something special.'}
+    ]
     return (
-        <h1>Hello Mouhcine from App file</h1>
+        <div>
+            <h1 className="blue">{title}</h1>
+            <p className="red">{paragraph}</p>
+            <h3>Comments ({comments.lenght}):</h3>
+            
+            <div>
+                <ul>
+                    {comments.map( comment => {
+                        <li>{comment.text}</li>
+                    })}   
+                </ul>
+
+            </div>
+            
+        </div> 
     )
 }
 
